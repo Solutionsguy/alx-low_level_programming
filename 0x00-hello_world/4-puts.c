a951ab93e84e2e4ce645f07bedb689982606bffd
solutionsguy <muthomicosmas1999@gmail.com>
2022-10-14 17:59:46 -0400

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 /**
  * *main - Entry
- * *Return: Always 0
+ *
+ * *Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
+
+}
