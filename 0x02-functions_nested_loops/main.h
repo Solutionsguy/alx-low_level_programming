63ed27b608b8eb35291ab0d025ccd89a789dd5c5
solutionsguy <muthomicosmas1999@gmail.com>
2022-10-18 18:50:41 -0400

    print putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,13 @@
-The prototypes of all your functions and the prototype of the function _putchar should be included in your header file called main.h
+int _putchar(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
