56b7a3a6671500c0c8f7d8103e6215d72e0f2262
solutionsguy <muthomicosmas1999@gmail.com>
2022-10-18 09:40:31 -0400

    prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#inint main(void)
+/**
+ * main - prints all numbers of base 16
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
