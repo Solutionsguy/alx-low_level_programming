421661a1a44e3ab8efbf23a18e1543a5802b0dfc
solutionsguy <muthomicosmas1999@gmail.com>
2022-10-18 20:06:02 -0400

    I play the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,9 @@
  * main - prints the alphabet in lowercase, followed by a new line
  * Return: 0
  * void print_alphabet
- * */
+ */
 void print_alphabet(void)
+	//print_alphabet - prints alphabets in lowercase
 {
 	char letter;
 
