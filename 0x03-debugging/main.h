59a26e8b2b8c8cc640941e2affc59bc74b2fb3d7
solutionsguy <muthomicosmas1999@gmail.com>
2022-10-21 19:11:35 -0400

    Trial

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main.h - contains prototypes for all functions in the task
+ *
+ */
+void postitive_or_negative(void)
+void largest_number(void)
+void print_remaining_days(void)
+
+#endif
