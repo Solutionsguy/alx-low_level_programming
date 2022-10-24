bd6ea332063b97d7db5f391ecfdb0a83ef5f4926
solutionsguy <muthomicosmas1999@gmail.com>
2022-10-24 17:50:29 -0400

    correction

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,15 @@
 #include <unistd.h>
 /**
  * _isdigit - function that checks for adigit 0 through 9
- * _putchar - writes charater to standard output
+ * @c: int to be checked
  * Return: 1
  */
 int _isdigit(int c)
 {
+	int y = 0;
+
 	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return(0);
+		y = 1;
+
+	return (y);
 }
