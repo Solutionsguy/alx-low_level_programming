e433d4f8c4c40465e5988ba79a6f7181c1ee6beb
solutionsguy <muthomicosmas1999@gmail.com>
2022-10-26 21:28:36 -0400

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,48 +4,23 @@
  * @s: string to be converted
  * Return: integer value to be converted
  */
-
-int main(void)
+int _atoi(char *s)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	int sign = 1;
+	unsigned int num = 0;
 
-	srand(time(0));
+	do {
+		if (*s == '-')
+			sign *= -1;
 
-	while (sum < 2772)
-	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 
-	password[index] = '\0';
+		else if (num > 0)
+			break;
 
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
+	} while (*s++);
 
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half1;
-				break;
-			}
-		}
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
-		}
-	}
-
-	printf("%s", password);
-
-	return (0);
+	return (num * sign);
 }
+
