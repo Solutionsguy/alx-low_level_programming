367b9494c98613a58ba844aee1c959e3f8ca683f
solutionsguy <muthomicosmas1999@gmail.com>
2022-10-29 15:59:43 -0400

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,33 @@
 #include "main.h"
-
 /**
-<<<<<<< HEAD
- * strcat - Concatenates the string pointed to by @src, including the
- *  terminating
- *  null byte, to the end of the string pointed to by @dest.
-=======
- * strcat - Concatenates the string pointed to by @src, including the terminating
->>>>>>> daedd19d89576feba8e3b87b9b23546ea94c51c1
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ * _strcat - concatinate the values of two integers.
  *
- * Return: A pointer to the destination string @dest.
+ * @dest: arg a
+ *
+ * @src: arg b
+ *
+ * Return: void.
  */
-<<<<<<< HEAD
-=======
 
->>>>>>> daedd19d89576feba8e3b87b9b23546ea94c51c1
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i, j;
 
-	while (dest[index++])
-		dest_len++;
+	i = 0;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
