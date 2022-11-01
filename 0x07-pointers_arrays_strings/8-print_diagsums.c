bc0acfb27d9180755bf179778bfbb3447f4c8d4f
solutionsguy <muthomicosmas1999@gmail.com>
2022-11-01 16:41:52 -0400

    pointers an arrays

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,22 +9,19 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int index, sum1 = 0, sum2 = 0;
+	int primC, sum1, sum2, diagC1, diagC2, row;
 
-	for (index = 0; index < size; index++)
+	primC = 0, sum1 = 0, sum2 = 0, diagC1 = 0, diagC2 = 0, row = 0;
+	while (primC < size * size)
 	{
-		sum1 += a[index];
-		a += size;
+		if (primC == diagC1 + (row * size))
+			sum1 += a[primC];
+		if (primC == (size - diagC2 - 1) + (row * size))
+			sum2 += a[primC];
+		if (((primC + 1) % size) == 0)
+			row++, diagC1++, diagC2++;
+		primC++;
 	}
-
-	a -= size;
-
-	for (index = 0; index < size; index++)
-	{
-		sum2 += a[index];
-		a -= size;
-	}
-
-	printf("%d, %d\n", sum1, sum2);
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
 }
-
